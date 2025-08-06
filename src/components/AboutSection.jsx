08e3411c488101fb66abb00a09fbdca7b62dcfda
Gaurav Negi <gauravnegi6987@gmail.com>
2025-08-06 23:55:32 +0530

    link added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1yHYLlKrKrqOd1jrkCUPIJislMsrCZJuU/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
